543c05115b5a9675014895a4464daafcb93caa1e
Wayne Roddy <dubrod@icloud.com>
2020-03-05 13:46:36 -0600

    Create AgeGate.php

---
+++ app/code/Vendor/AgeGate/Block/AgeGate.php
@@ -0,0 +1,9 @@
+<?php
+namespace Vendor\AgeGate\Block;
+class AgeGate extends \Magento\Framework\View\Element\Template
+{
+    public function getContent() : string
+    {
+        return 'Dummy content';
+    }
+}
