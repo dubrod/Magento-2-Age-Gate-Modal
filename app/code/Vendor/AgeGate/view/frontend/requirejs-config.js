5260753b51fa7998f6948330178ae442eb2aa368
Wayne Roddy <dubrod@icloud.com>
2020-03-05 13:49:13 -0600

    Create requirejs-config.js

---
+++ app/code/Vendor/AgeGate/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'agegate-modal': 'Vendor_AgeGate/js/agegate-modal',
+            'agegate-check': 'Vendor_AgeGate/js/agegate-check'
+        }
+    }
+};
